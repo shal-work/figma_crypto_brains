bc4e37174e087069e54030b2e3261bcc001ee311
Shal <shal.work@mail.ru>
2024-08-11 18:19:51 +0300

    fourth commit, fixed products docs

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,6 @@
 'use strict';
 import $ from './lib/lib';
 import './lib/site/main.js';
-// src\js\lib\site\main.js
 
 
 console.log("%c  РОССИЯ  ", "background: blue; color: yellow; font-size: x-large; border-left: 5px solid black; border-top: 30px solid white; border-right: 2px solid black; border-bottom: 30px solid red;");
-
-// window.addEventListener('DOMContentLoaded', () => {
-//     let input = document.getElementById('user_password');
-//     input.addEventListener('click', function(e) {
-//         $('.eye-closed').hide();
-//         console.log(66666);
-//     });
-//     input.addEventListener('blur', function(e) {
-//         console.log(8888);
-//         $('.eye-closed').show();
-//     });
-// });
